cafedee0137c1793819d8165311dafadb72c4535
matt-letts <yv22008@bristol.ac.uk>
2026-02-23 13:07:10 +0000

    adding codelists

--- analysis/dataset_definition/codelists.py
+++ analysis/dataset_definition/codelists.py
@@ -1,48 +1,96 @@
 # This script retrieves CSV files from codelists folder and stores them as variables
 
-from ehrql import codelist_from_csv
+from ehrql import codelist, codelist_from_csv, combine_codelists
 
-# Shalini (https://bmjmedicine.bmj.com/content/3/1/e000807):
-# Defined KRT using existing primary care codelists, and developed secondary care codelists.
-# split into a different modalities and 'kidney replacement therapy' for when unclear
+### kidney replacement therapy codelists same methods as in this paper: https://bmjmedicine.bmj.com/content/3/1/e000807
 
-# will copy their coding structure which is in their supplement
 
-### primary care coding for KRT pre-baseline
-# opensafely/kidney-transplant/21865860 
-# opensafely/dialysis/3ce108ac #
-# opensafely/renal-replacement-therapy/5b5ed963 # full list of KRT codes
+## primary care codes
 
-### secondary care coding for KRT pre-baseline
-# ukrr/dialysis-opcs-4/505bef04 # dialysis
-# ukrr/dialysis-icd10/2a5cf9a2/ # dialysis
-# ICD10 Z940 - kidney transplant status # transplant
-# user/viyaasan/kidney-transplant-opcs-4/18d788ae/ # transplant
-# Other codes for 'KRT' needed 
-# ICD10: 
-# T861 - kidney transplant failure and rejection
-# OPCS4: 
-# M023 - bilateral nephrectomy; 
-# M026 - excision of rejected transplanted kidney; 
-# M027 - excision of transplanted kidney NEC; 
-# X412 - removal of ambulatory peritoneal dialysis catheter
+# primary care ctv3 codes - dialysis, ktx then all krt
 
-# Viyassan used this codelist to generate creatinine values
-#user/bangzheng/creatinine-value/7d319079
-
-# primary care CTV3 codes - initially all KRT, then dialysis then Ktx
-primary_care_krt_codes = codelist_from_csv(
-    "codelists/opensafely-renal-replacement-therapy.csv",
-    system="ctv3",
-    column="CTV3ID"
-)
 primary_care_dialysis_codes = codelist_from_csv(
     "codelists/opensafely-dialysis.csv",
     system="ctv3",
     column="CTV3ID"
 )
-primary_care_kidney_transplant_codes = codelist_from_csv(
+
+primary_care_ktx_codes = codelist_from_csv(
     "codelists/opensafely-kidney-transplant.csv",
     system="ctv3",
     column="CTV3ID"
+)
+
+primary_care_krt_codes = codelist_from_csv(
+    "codelists/opensafely-renal-replacement-therapy.csv",
+    system="ctv3",
+    column="CTV3ID"
+)
+
+# combine them altogether for full list of primary care codes 
+
+primary_care_krt_codes_all = combine_codelists(
+    primary_care_dialysis_codes,
+    primary_care_ktx_codes,
+    primary_care_krt_codes
+)
+
+# secondary care icd10 codes - dialysis, ktx then all krt
+
+secondary_care_dialysis_codes_icd10 = codelist_from_csv(
+    "codelists/ukrr-dialysis-icd10.csv",
+    system="icd10",
+    column="code"
+)
+
+secondary_care_ktx_codes_icd10 = codelist(["Z940"], system="icd10")
+
+secondary_care_krt_codes_icd10 = combine_codelists(
+    secondary_care_dialysis_codes_icd10,
+    secondary_care_ktx_codes_icd10,
+    codelist(["T861"], system="icd10") 
+    # T861 = transplant failure; kidney failure but unknown treatment modality
+)
+
+# secondary care opcs4 codes - dialysis, ktx then all krt
+
+secondary_care_dialysis_codes_opcs4 = codelist_from_csv(
+    "codelists/ukrr-dialysis-opcs-4.csv",
+    system="opcs4",
+    column="code"
+)
+
+secondary_care_ktx_codes_opcs4 = codelist_from_csv(
+    "codelists/user-viyaasan-kidney-transplant-opcs-4.csv",
+    system="opcs4",
+    column="code"
+)
+
+secondary_care_krt_codes_opcs4 = combine_codelists(
+    secondary_care_dialysis_codes_opcs4,
+    secondary_care_ktx_codes_opcs4,
+    codelist(["M023", "M026", "M027", "X412"], system="opcs4"), 
+    # all indicating kidney failure but unknown treatment modality
+    # M023 - bilateral nephrectomy; 
+    # M026 - excision of rejected transplanted kidney; 
+    # M027 - excision of transplanted kidney NEC; 
+    # X412 - removal of ambulatory peritoneal dialysis catheter
+)
+
+# not actually sure I will need all of these categorisations 
+
+secondary_care_dialysis_codes_all = combine_codelists(
+    secondary_care_dialysis_codes_opcs4,
+    secondary_care_dialysis_codes_icd10
+)
+
+secondary_care_ktx_codes_all = combine_codelists(
+    secondary_care_ktx_codes_opcs4,
+    secondary_care_ktx_codes_icd10
+)
+
+# this last one contains all of the secondary care krt/dialysis/transplant codes
+seoncdary_care_krt_codes_all = combine_codelists(
+    secondary_care_krt_codes_opcs4,
+    secondary_care_krt_codes_icd10
 )
