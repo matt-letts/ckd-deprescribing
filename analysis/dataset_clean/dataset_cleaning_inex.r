2623678560080eb1136cac65fbbffe0e79409df9
matt-letts <yv22008@bristol.ac.uk>
2026-03-13 14:55:43 +0000

    CKD/KRT inex added to main cleaning script

--- analysis/dataset_clean/dataset_cleaning_inex.r
+++ analysis/dataset_clean/dataset_cleaning_inex.r
@@ -1,4 +1,6 @@
 ##### describe this script #####
+##### I somehow want to put the original dataset.arrow into the output/data folder
+#####
 
 message("Import libraries and functions \n")
 library(fs)
@@ -8,25 +10,21 @@ library(arrow)
 library(dplyr)
 library(lubridate)
 source(here::here("analysis", "functions", "fn_transform_variables.r"))
-source(here::here("analysis", "functions", "fn_build_flow_counts.r"))
-source(here::here("analysis", "functions", "fn_describe_data.r"))
-source(here::here("analysis", "functions", "fn_describe_and_flow.r"))
+source(here::here("analysis", "functions", "fn_data_describing.r"))
 source(here::here("analysis", "functions", "fn_disclosure_control.r"))
 source(here::here("analysis", "functions", "fn_qa.r"))
 source(here::here("analysis", "functions", "fn_dem_inex_criteria.r"))
 source(here::here("analysis", "functions", "fn_ckd_inex_criteria.r"))
 
-
-message("Create output folder \n")
+message("Create output folders")
 dir_create(here::here("output", "data"))
 dir_create(here::here("output", "data_descriptions"))
 
-message("Import dates \n")
+message("Import dates")
 source(here::here("analysis", "dataset_definition", "study_dates.r"))
 study_dates <- lapply(study_dates, function(x) as.Date(x))
 
-
-message("Process the dataset lazily \n")
+message("Process the dataset lazily")
 
 input_filename = "dataset.arrow"
 
@@ -57,12 +55,28 @@ dataset_cleaning_4_demographic_inex_applied <- fn_dem_inex_criteria(
 )
 
 # apply CKD inclusion criteria
+# 4 new variables added to data:
+# - num_egfr_1 - numerical value of most recent eGFR
+# - num_egfr_2 - numerical value of most recent eGFR 90+ days prior to num_egfr_1
+# - bin_has_ckd45_by_scr - boolean TRUE if eGFRs consistent with CKD G4 or G5
+# - cat_ckd_stage_by_scr - category of eGFR derived CKD (G4, G5, G4/G5, or no G4/G5)
 dataset_cleaning_5_ckd_inex_applied <- fn_ckd_inex_criteria(
   arrow_data = dataset_cleaning_4_demographic_inex_applied,
   rounding_threshold = 6,
   collect_and_describe = FALSE,
   index_date = study_dates$index_date
 )
+
+# apply KRT exclusion criteria
+dataset_cleaning_6_krt_inex_applied <- fn_krt_inex_criteria(
+  arrow_data = dataset_cleaning_5_ckd_inex_applied,
+  rounding_threshold = 6,
+  collect_and_describe = FALSE,
+  krt_source = "primary"
+)
+
+## I don't think that there will be any missing data to handle...
+## and there are no categorical data to reframe
 
 # write all datasets to .txt and flow dataframe
 flow <- describe_and_flow(
