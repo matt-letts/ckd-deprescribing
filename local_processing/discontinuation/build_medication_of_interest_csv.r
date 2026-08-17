5e914ef251cd29d3794cc7e41b83abed4ad4e716
matt-letts <yv22008@bristol.ac.uk>
2026-08-17 13:52:28 +0100

    pulled back to more straightforward codelist pipeline

---
+++ local_processing/discontinuation/build_medication_of_interest_csv.r
@@ -0,0 +1,28 @@
+###################################################################################
+# This script builds analysis/config/medication_of_interest.csv - the central
+# reference of medications of interest and their OpenCodelists dm+d codelists
+#
+# Runs outside the OpenSAFELY pipeline
+###################################################################################
+
+# Import libraries --------------------------------------------------------------
+library(here)
+library(tidyverse)
+
+# Define medications of interest -----------------------------------------
+# codelist_path must exist in codelists/
+
+medication_of_interest_list <- c(
+  statins = "codelists/bristol-statins-dmd.csv"
+)
+
+# Build csv that contains central references of medications of interest ----------
+result <- tibble(
+  name = names(medication_of_interest_list),
+  codelist_path = medication_of_interest_list
+)
+
+write_csv(
+  result,
+  here::here("analysis", "config", "medication_of_interest.csv")
+)
